8568d794e06448fbfa756e056b9cc798f1651cb3
echo <echofoxey@gmail.com>
2023-01-19 16:02:07 +0100

    portal sound

--- Tower Of Trials/Assets/Scripts/Portal.cs
+++ Tower Of Trials/Assets/Scripts/Portal.cs
@@ -7,9 +7,22 @@ public class Portal : MonoBehaviour
 {
     Animator anim;
     bool isOpen;
+
+    #region FMOD
+
+    private FMOD.Studio.EventInstance portalInstance;
+
+    #endregion
+
     void Awake()
     {
         anim = GetComponent<Animator>();
+
+        #region FMOD
+
+        portalInstance = FMODUnity.RuntimeManager.CreateInstance("event:/SFX/env/Portal");
+
+        #endregion
     }
     void Start()
     {
@@ -17,6 +30,8 @@ public class Portal : MonoBehaviour
     }
     void OpenPortal()
     {
+        portalInstance.start(); //FMOD sfx
+
         anim.SetTrigger("OpenPortal");
         isOpen = true;
     }
